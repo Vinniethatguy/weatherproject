28e3e12b0f98de9691cf159bedb9f4dc97b223b3
Vince Smith <vincesmith514@gmail.com>
2023-04-24 16:20:54 -0500

    Navbar

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { BrowserRouter as  Link } from 'react-router-dom';
+
 
 function App() {
   const [data, setData] = useState([]);
@@ -17,9 +19,28 @@ function App() {
 
   return (
     <div>
-      <h1>Posts</h1>
-       <ul>
-      </ul>
+      
+      <header>
+				<nav className='nav'>
+					<a className = 'title' href='/'>Weather App</a>
+					<div className="header">
+						<ul className="nav-items">
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/login">Login</Link>
+						</li>
+						<li>
+							<Link to="/Register">Register</Link>
+						</li>
+						<li>
+							<Link to="/User_profile">User Profile</Link>
+						</li>
+						</ul>
+					</div>
+				</nav>
+			</header>
     </div>
   );
 }
