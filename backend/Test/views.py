1627851487847840176697be04e86d143e963805
Vince Smith <vincesmith514@gmail.com>
2023-04-26 14:42:37 -0500

    Views Features

--- backend/Test/views.py
+++ backend/Test/views.py
@@ -10,6 +10,17 @@ class Test(APIView):
                 "photo_id" : "https://images.pexels.com/photos/3184360/pexels-photo-3184360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             },
             "id" : "2",
-            "title" : "Person"
+            "State": "MO",
+            "City" : "Kansas City",
+            "Zip Code": "64133",
+            "Temperature": "70",
+            "Feels Like": "55",
+            "Precipitation" : "0%",
+            "Humidity": "21%",
+            "Wind":"10mph",
+            "Date": "April 24",
+            "Time": "10:00",
+            "Forcast": "Sunny"
+            
           }
         return Response(data)
