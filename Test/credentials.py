a51e3e089858c0b492d84b71953049199bf4257c
Pixler132 <61323384+Pixler132@users.noreply.github.com>
2023-05-17 17:32:52 -0500

    cred

--- Test/credentials.py
+++ Test/credentials.py
@@ -1,5 +1,5 @@
 def get_api_key():
     # api_key should be equal to https://openweathermap.org key provided by site
     # blank by default
-    api_key = '31c1b85feece2a201b5d574fad690f86'
+    api_key = ''
     return api_key
