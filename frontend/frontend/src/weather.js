d0f622d9dff61946a4bbc73cc876d4ddf32386bb
Vince Smith <vincesmith514@gmail.com>
2023-05-31 20:46:15 -0400

    Search, time and sizing

--- frontend/frontend/src/weather.js
+++ frontend/frontend/src/weather.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import sunnyImage from './sunny.svg';
-import Card from 'react-bootstrap/Card';
 
 function Weather({ location, dayData }) {
   return (
     <Container>
       <Row>
         <Col className='weather_card' xs={2} order-first>
-          {/* Display weather information */}
           <span>{dayData[0]}</span>
           <br />
           <span>{dayData[1]}</span>
@@ -20,8 +18,10 @@ function Weather({ location, dayData }) {
           <span className='temp'>{dayData[2]}</span>
         </Col>
         <Col className='weekly_forcast' order-last>
-        <Col className="rectangle-column"></Col>
-        <Col className='search bar'> Change Location</Col>
+        <Col className="rectangle-column">
+          
+        </Col>
+        <input className="search-bar" type="search" />
         </Col>
         
       </Row>
