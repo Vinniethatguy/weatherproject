5068911567a4be1eefb0c1f39d165d386a4c0a5d
Vince Smith <vincesmith514@gmail.com>
2023-06-07 22:44:56 -0400

    CSS, name changes

--- frontend/frontend/src/weather.js
+++ frontend/frontend/src/weather.js
@@ -21,10 +21,11 @@ function Weather({ location, dayData }) {
               src={currentDay[5]}
               alt=''
               style={{ width: '100px', height: '100px' }}/>
+              <span className='temp'>{currentDay[3]}</span>
           </div>
         </Col>
         <Col className='weekly_forcast' order-last>
-          <div className="image-container">
+          <div className="weekly-container">
             {nextFourDays.map((day, index) => (
               <div key={index} className="image-wrapper">
                 <span>{day[0]}</span>
@@ -37,14 +38,15 @@ function Weather({ location, dayData }) {
                   className="weather-image"
                   src={day[5]}
                   alt=''
-                  style={{ width: '100px', height: '100px' }} 
+                  style={{ width: '100px', height: '100px', color: "white" }} 
                 />
                 <br />
                 <span className='temp'>{day[2]}</span> 
                 <br />
                 <span className='weather'>{day[3]}</span> 
                 <br />
-                <span className='temp'>{currentDay[3]}</span>
+
+                
                 <hr />
               </div>
             ))}
