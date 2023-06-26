fc2bfcfe9bbab914a62088e800397eebc57a7843
Vince Smith <vincesmith514@gmail.com>
2023-06-26 17:57:24 -0400

    Search bar

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Header from './header';
 import Weather from './weather';
+
+function buttonZip() {
+  console.log("hello")
+}
 
 function App() {
   const [location, setLocation] = useState('');
   const [weatherData, setWeatherData] = useState([]);
+  let [zipCode, setZipCode] = useState('64109');
+  let [changeZip] = '1';
 
-  useEffect(() => {
-    axios.get('http://localhost:4000/weather/data/64109/')
+
+  let handleZipCodeChange = (event) => {
+    setZipCode(event.target.value);
+  };
+
+  let handleSearch = () => {
+    axios.get(`http://localhost:4000/weather/data/${zipCode}/`)
       .then(response => {
         console.log(response.data);
         const { location, days } = response.data.weatherInfo;
@@ -18,17 +28,41 @@ function App() {
       .catch(error => {
         console.log(error);
       });
-  }, []);
+  };
+
+  useEffect(() => {
+    axios.get(`http://localhost:4000/weather/data/${zipCode}/`)
+      .then(response => {
+        console.log(response.data);
+        const { location, days } = response.data.weatherInfo;
+        setLocation(location);
+        setWeatherData(days);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, [zipCode]);
 
   return (
     <div>
-      <Header />
       <br />
       {location && weatherData.length > 0 && (
         <div>
           <Weather location={location} dayData={weatherData} />
         </div>
       )}
+      <div>
+      
+        <input
+          type="text"
+          placeholder="Change Zip Code"
+          value={changeZip}
+          onChange={handleZipCodeChange}
+          className='search_bar'
+        />
+        <button onClick={console.log("hello world")}></button>
+        
+      </div>
     </div>
   );
 }
--- frontend/frontend/src/weather.js
+++ frontend/frontend/src/weather.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
 
 function Weather({ location, dayData }) {
-  const currentDay = dayData[0]; 
-  const nextFourDays = dayData.slice(1, 6); 
+  const currentDay = dayData[0];
+  const nextFourDays = dayData.slice(1, 6);
 
   return (
     <Container>
       <Row>
-        <Col className='weather_card' xs={2} order-first>
+        <Col className='weather_card' xs={2} orderFirst>
           <div className='weather_info'>
             <span>{currentDay[0]}</span>
             <br />
@@ -16,42 +17,33 @@ function Weather({ location, dayData }) {
             <br />
             <span>{location}</span>
             <br />
-            <img
-              className="weather-image"
-              src={currentDay[5]}
-              alt=''/>
-              <span className='temp'>{currentDay[3]}</span>
+            <img className="weather-image" src={currentDay[5]} alt='' />
+            <span className='temp'>{currentDay[3]}</span>
           </div>
         </Col>
-        <Col className='weekly_forcast' order-last>
+        <Col className='weekly_forcast' orderLast>
           <div className="weekly-container">
             {nextFourDays.map((day, index) => (
               <div key={index} className="image-wrapper">
-                <span>{day[0]}</span>
-                <br />
-                <span>{day[1]}</span>
-                <br />
-                <span>{location}</span>
-                <br />
-                <img
-                  className="weekly-images"
-                  src={day[5]}
-                  alt=''
-                />
-                <br />
-                <span className='temp'>{day[2]}</span> 
-                <br />
-                <span className='weather'>L: {day[3]}</span> 
-                <br></br>
-                <span className='weather'>H {day[4]}</span> 
-                <br />
-                <p>Hello
-                  Hello
-                </p>
+                <Card style={{ width: '8rem', backgroundColor: '#005F73', color: '#FFFFFF' }}>
+                  <span>{day[0]}</span>
+                  <br />
+                  <span>{day[1]}</span>
+                  <br />
+                  <span>{location}</span>
+                  <br />
+                  <img className="weekly-images" src={day[5]} alt=''  />
+                  <br />
+                  <span className='temp'>{day[2]}</span>
+                  <br />
+                  <span className='weather'>L: {day[3]}</span>
+                  <br />
+                  <span className='weather'>H {day[4]}</span>
+                  <br />
+                </Card>
               </div>
             ))}
           </div>
-
         </Col>
       </Row>
     </Container>
