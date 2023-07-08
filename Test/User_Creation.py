308affd80244d53227907982b96ae6532ebfd240
Pixler132 <61323384+Pixler132@users.noreply.github.com>
2023-07-08 16:52:23 -0500

    made login and registration

--- Test/User_Creation.py
+++ Test/User_Creation.py
@@ -1,8 +1,23 @@
 import requests
-def main():
-    response = requests.get("http://127.0.0.1:4000/user/register/")
+
+def register_user():
+    user_object = {"username": "Steve1",
+                   "password": "SuperPass2325!",
+                   "passcheck": "SuperPass2325!"}
+    response = requests.post("http://127.0.0.1:4000/user/register/", data=user_object)
     print(response.status_code)
     print(response.content)
+
+def login_user():
+    user_object = {"username" : "Pixler",
+                   "password" : "abc123" }
+    response = requests.post("http://127.0.0.1:4000/user/login/", data=user_object)
+    print(response.status_code)
+    print(response.content)
+
+
+def main():
+    login_user()
 
 
 if __name__ == "__main__":
--- backend/UserManagement/views.py
+++ backend/UserManagement/views.py
@@ -1,12 +1,46 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
 
 
 class LoginView(APIView):
     def get(self, request):
         return Response("hello from login")
+    
+    def post(self, request):
+        print(request.data)
+        username = request.data['username']
+        password = request.data["password"]
+        userObjects = User.objects.filter(username =username)
+        if len(userObjects) == 1 and userObjects[0].check_password(password):
+            return Response({"username": username})
+        else:
+            return Response({"errors": "Improper login"})
 
 class RegisterView(APIView):
     def get(self, request):
         return Response("hello from Register")
+    
+    def post(self, request):
+        user_data = dict(request.data)
+        if user_data['password'] != user_data['passcheck']:
+            return Response("Mismatch password")
+       
+        userInfo = {
+                'username' :user_data['username'][0],
+                'email': "anon@anon.com",
+                'password' : user_data['password'][0],
+                'first_name' : "N/A",
+                'last_name' : "N/A"
+            }
+       
+        userSerializer = UserSerializer(data=userInfo)
+        if userSerializer.is_valid():
+            userSerializer.save()
+            return Response("Proper user Data given")
+        else:
+            print(userSerializer.error_messages)
+            print(userSerializer.errors)
+            return Response(userSerializer.errors)
